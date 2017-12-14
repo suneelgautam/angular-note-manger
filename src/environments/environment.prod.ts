62ffc24235f85deabb700c3c595cb9525ee86f57
SuneelGautam <sunilgautam63@hotmail.com>
2017-12-14 15:54:11 +0530

    angular note manger
    simple note manger

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDQN7Aurz06zfjGv4v0TkHwH95NrtpJlvw",
+    authDomain: "angular-project-84fa1.firebaseapp.com",
+    databaseURL: "https://angular-project-84fa1.firebaseio.com",
+    projectId: "angular-project-84fa1",
+    storageBucket: "angular-project-84fa1.appspot.com",
+    messagingSenderId: "383536859800"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDQN7Aurz06zfjGv4v0TkHwH95NrtpJlvw",
+    authDomain: "angular-project-84fa1.firebaseapp.com",
+    databaseURL: "https://angular-project-84fa1.firebaseio.com",
+    projectId: "angular-project-84fa1",
+    storageBucket: "angular-project-84fa1.appspot.com",
+    messagingSenderId: "383536859800"
+  }
+};
